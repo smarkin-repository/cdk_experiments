f3fc2a68d5d4ce2998a331b7beb71bf1a7685e7a
Markin Sergey <ssmarkin.public@gmail.com>
2023-11-20 10:04:31 +0100

    [=] support docker

--- ec2spots_workshop/data/scripts/user_data.sh
+++ ec2spots_workshop/data/scripts/user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo yum install -y httpd python3 stress-ng
+sudo yum install -y httpd python3 stress-ng docker
 
 sudo sed -i 's/^Listen [0-9]\+/Listen 8080/' /etc/httpd/conf/httpd.conf
 sudo systemctl start httpd
@@ -16,3 +16,15 @@ sudo curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 sudo python3 /tmp/get-pip.py
 
 pip3 install boto3
+
+# Install Docker
+sudo usermod -a -G docker ec2-user
+id ec2-user
+newgrp docker
+
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 500480925365.dkr.ecr.us-east-1.amazonaws.com
+sudo docker pull 500480925365.dkr.ecr.us-east-1.amazonaws.com/base-repo:latest
+
